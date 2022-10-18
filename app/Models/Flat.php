3526856b359022e41cfc1279f22fc1ad4d32a758
tarex7 <tarex7@gmail.com>
2022-10-18 16:57:33 +0200

    added messages table

--- app/Models/Flat.php
+++ app/Models/Flat.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flat extends Model
 {
+    //Relazione 1 to 1 con views
     public function flatViews() {
         return $this->hasOne('App\Models\View');
+    }
+
+    //Relazione 1 to many con views
+
+    public function messages() {
+        return $this->hasMany('App\Models\Message');
     }
 }
