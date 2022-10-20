0b215ef3e1c348c4b9fda8c41c7f0125907d90fe
Eikcaj1998 <jw220998@gmail.com>
2022-10-20 17:26:05 +0200

    test

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -1,95 +1,40 @@
 @extends('layouts.app')
-
+@include('includes.header')
 @section('content')
-    <header class="d-flex justify-content-between align-items-center">
-        <h1>Lista Appartamenti</h1>
 
-
-        <div class="d-flex justify-content-end align-items-center">
-
-
-            {{-- Add new Flat --}}
-            <a class='btn btn-success'href="{{ route('admin.flats.create') }}">
-                <i class='fa-solid fa-plus mr-2'></i> Nuovo
-                Appartamneto</a>
-
-            <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 wrapper d-flex flex-wrap">
+                @forelse ($flats as $flat)
+                    <div class="flat-card bg-dark me-2">
+                        {{-- <img src="{{ $comic->thumb }}"alt=""> --}}
+                        <div class="imaginary"></div>
+                        <div class="appair">
+                            <div class="d-flex flex-column align-items-center">
+                                <a class="btn btn-primary text-light my-2 p-1 "
+                                    href="{{ route('admin.flats.show', $flat->id) }}">
+                                    Visualizza
+                                </a>
+                                <a class="btn bg-secondary text-light my-2 p-1"
+                                    href="{{ route('admin.flats.edit', $flat->id) }}">
+                                    Modifica
+                                </a>
+                                <form action="{{ route('admin.flats.destroy', $flat->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn bg-danger text-light my-2 p-1"> Elimina</button>
+                                </form>
+                            </div>
+                        </div>
+                        <h5 class="fw-bold text-light mt-3 fs-6">{{ $flat->title }}</h5>
+                    </div>
+                @empty
+                    <tr>
+                        <td colspan="8">
+                            <h3 class="text-center">Nessun Appartamneto</h3>
+                        </td>
+                    </tr>
+                @endforelse
             </div>
         </div>
-
-    </header>
-
-
-    <table class="table table-striped table-dark">
-        <thead>
-            <tr class="text-center">
-                <th scope="col">#</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Numero Camere</th>
-                <th scope="col">Numeri Letti </th>
-                <th scope="col">Numeri bagni </th>
-                <th scope="col">Indirizzo</th>
-                <th scope="col">Latitudine</th>
-                <th scope="col">Longitudine</th>
-                <th scope="col">Visibile</th>
-                <th scope="col" class="text-center">Azioni</th>
-
-
-                </th>
-
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($flats as $flat)
-                <tr class=text-center>
-                    <th scope="row">{{ $flat->id }}</th>
-                    <td>{{ $flat->title }}</td>
-                    <td>{{ $flat->room_number }}</td>
-                    <td>{{ $flat->bed_number }}</td>
-                    <td>{{ $flat->bathroom_number }}</td>
-                    <td>{{ $flat->address }}</td>
-                    <td>{{ $flat->latitude }}</td>
-                    <td>{{ $flat->longitude }}</td>
-                    <td>{{ $flat->visible }}</td>
-
-                    <td class="p-1">
-
-                        <form action="{{ route('admin.flats.destroy', $flat->id) }}" method="POST" class="delete-form"> 
-
-                        
-                            <a class='btn btn-sm btn-primary mr-2' href="{{ route('admin.flats.show', $flat) }}"><i
-                                    class='fa-solid fa-eye'></i>vedi</a>
-                        
-                            <a class="btn btn-sm btn-warning" href="{{ route('admin.flats.edit', $flat) }}"><i
-                                    class="fa-solid fa-pencil"></i>Edit </a>
-
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-sm btn-danger ml-2" type="submit">
-                                <i class="fa-solid fa-trash"></i>Elimina
-                            </button> 
-
-                        {{-- @endif --}}
-                        </form> 
-
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="8">
-                        <h3 class="text-center">Nessun Appartamneto</h3>
-                    </td>
-                </tr>
-            @endforelse
-
-        </tbody>
-    </table>
-
-
-    @foreach ($flats as $flat)
-        <p>{{ $flat->title }}</p>
-        <p>{{ $flat->room_number }}</p>
-    @endforeach
-
-    </section>
-@endsection
+    </div>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,38 @@
+<header>
+    <div class="container py-2">
+        <div class="d-flex justify-content-between align-items-center py-4">
+            <img class="img fluid col-2" src={{ asset('images/logo.png') }} alt="logo Air BnB">
+            <nav>
+                <ul>
+                    <li class="fw-bold"><a>Characters</a></li>
+                    <li class="fw-bold"><a href="#">Comics</a></li>
+                    <li class="fw-bold"><a>Movies</a></li>
+                    <li class="fw-bold"><a>Tv</a></li>
+                    <li class="fw-bold"><a>Games</a></li>
+                    <li class="fw-bold"><a>Collectibles</a></li>
+                    <li class="fw-bold"><a>Videos</a></li>
+                    <li class="fw-bold"><a>Fans</a></li>
+                    <li class="fw-bold"><a>News</a></li>
+                    <li class="fw-bold"><a>Shop</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="d-flex justify-content-between align-items-center">
+            <h1>Lista Appartamenti</h1>
+    
+    
+            <div class="d-flex justify-content-end align-items-center">
+    
+    
+                {{-- Add new Flat --}}
+                <a class='btn btn-success'href="{{ route('admin.flats.create') }}">
+                    <i class='fa-solid fa-plus mr-2'></i> Nuovo
+                    Appartamneto</a>
+    
+                <div>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+</header>
