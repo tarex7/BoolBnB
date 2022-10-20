e6bb2ea0e7818206fccf156859d8612f893b9e28
torone89 <torone89>
2022-10-20 19:01:05 +0200

    feat: i vue router /   </router-link ok

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,14 +1,19 @@
 require("./bootstrap");
 
-window.Vue = require("vue");
+
+window.Vue = require('vue');
 
 // IMPORTO BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.css';
+import router from './router.js'
+import App from './components/App.vue'
+import AppLoader from './components/AppLoader'
+import AppAlert from './components/AppAlert.vue'
 
-import App from "./components/App.vue";
-
+Vue.component('AppLoader', AppLoader)
+Vue.component('AppAlert', AppAlert)
 const root = new Vue({
-    //router,
-    el: "#root",
-    render: (h) => h(App),
-});
+    router,
+    el: '#root',
+    render: h => h(App)
+})
