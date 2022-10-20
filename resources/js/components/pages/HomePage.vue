7309d66ea25b6ffecc6a3474ada5dd83c8ebc068
torone89 <torone89>
2022-10-20 17:24:16 +0200

    feat: import Flatlist in Homepage

---
+++ resources/js/components/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home-page">
+    stampo tutti gli appartamenti!!!!!
+    <flat-list />
+  </div>
+</template>
+
+<script>
+import FlatList from "../flats/FlatList.vue";
+export default {
+  name: "HomePage",
+  components: { FlatList },
+};
+</script>
+
+<style>
+</style>
