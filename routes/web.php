6345086090cb121628a1b4cc4939a5f6de91cc50
tarex7 <tarex7@gmail.com>
2022-10-20 10:58:59 +0200

    created destroy/show

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::middleware('auth')
+->prefix('admin')
+->namespace('admin')
+->name('admin.')
+->group(function(){
+    Route::resource('flats','FlatController');
+
+});
 
 Route::get('/', function () {
     return view('guest.home');
 });
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('flats','FlatController');
