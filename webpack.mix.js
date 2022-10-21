9c2b48b823400951c3d88b03eaca62bc32aca316
Eikcaj1998 <jw220998@gmail.com>
2022-10-21 16:24:46 +0200

    logout

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/front.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({ processCssUrls: true });
+    .sass('resources/sass/app.scss', 'public/css');
