5899333f7b6010faef6e862b8987d167bf9038c9
Eikcaj1998 <jw220998@gmail.com>
2022-10-22 11:15:36 +0200

    fix run watch log out

--- public/js/app.js
+++ public/js/app.js
@@ -49715,13 +49715,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! /Applications/MAMP/htdocs/boolbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
-=======
 __webpack_require__(/*! C:\laravel\boolbnb\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\laravel\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> b1d462e0a41b99a6ac385cfc68cffbdb43dd2f45
 
 
 /***/ })
