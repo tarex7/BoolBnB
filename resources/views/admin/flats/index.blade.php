c2c194b97d7214d8d32bf815f490f3039a4a0221
tarex7 <tarex7@gmail.com>
2022-10-23 14:01:50 +0200

    added buttons on show

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -3,17 +3,19 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12 wrapper d-flex flex-wrap">
+<div class="container">
+    <div class="row">
+            <div class="col-12 d-flex justify-content-center my-4 ">
+                <a href="{{ route('admin.flats.create') }}" class="btn btn-primary">Aggiungi un appartamento</a>
+            </div>
+            <div class="col-12 wrapper d-flex flex-wrap justify-content-center">
                 @forelse ($flats as $flat)
-                <div class="card m-3 p-3" style="width: 18rem;">
+                <div class="card m-1 p-3" style="width: 18rem;">
                     <a href="{{ Route('admin.flats.show', $flat->id) }}">
                         <img src="{{ $flat->image }}" class="card-img-top" alt="...">
                     </a>
                         <div class="card-body">
                           <h5 class="card-title">{{ $flat->title }}</h5>
-                          <p class="card-text"><strong>{{ $flat->price_per_day }} € </strong>notte</p>
                         </div>
                       </div>
 
