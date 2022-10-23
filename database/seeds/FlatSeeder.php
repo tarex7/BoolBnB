a8bb1843c63bbbb710a4636d84e612c47042317e
torone89 <torone89>
2022-10-23 14:08:05 +0200

    feat: Visible complete!ok !Need to fix message!

--- database/seeds/FlatSeeder.php
+++ database/seeds/FlatSeeder.php
@@ -3,7 +3,7 @@
 use App\User;
 use App\Models\Flat;
 use Illuminate\Support\Arr;
-use Faker\Generator as Faker; 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,33 +18,30 @@ class FlatSeeder extends Seeder
     {
         $user_ids = User::pluck('id')->toArray();
         $flats_imgs = config('flats_imgs');
-        for($i = 0; $i < 9 ; $i++)
-     {
-        $new_flat = new Flat();
-        $new_flat->user_id = Arr::random($user_ids);
-        $new_flat->user_id = 1;
-        $new_flat->title = $faker-> text(rand(30,60));
-        $new_flat->description = $faker->text(900);
-        $new_flat->price_per_day = $faker->randomFloat(2, 50, 2000);
-        $new_flat->square_mt = $faker->numberBetween(50, 1000);
-        $new_flat->room_number = $faker->numberBetween(1,50);
-        $new_flat->bed_number = $faker->numberBetween(1,10);
-        $new_flat->bathroom_number = $faker->numberBetween(1,10);
-        $new_flat->address = $faker->address();
-        $new_flat->latitude = bcdiv($faker->randomFloat(2, 40, 50),2,2);
-        $new_flat->longitude = $faker->randomFloat(2, 40, 50);
-        ;
-        
+        for ($i = 0; $i < 9; $i++) {
+            $new_flat = new Flat();
+            $new_flat->user_id = Arr::random($user_ids);
+            $new_flat->user_id = 1;
+            $new_flat->title = $faker->text(rand(30, 60));
+            $new_flat->description = $faker->text(900);
+            $new_flat->price_per_day = $faker->randomFloat(2, 50, 2000);
+            $new_flat->square_mt = $faker->numberBetween(50, 1000);
+            $new_flat->room_number = $faker->numberBetween(1, 50);
+            $new_flat->bed_number = $faker->numberBetween(1, 10);
+            $new_flat->bathroom_number = $faker->numberBetween(1, 10);
+            $new_flat->address = $faker->address();
+            $new_flat->latitude = bcdiv($faker->randomFloat(2, 40, 50), 2, 2);
+            $new_flat->longitude = $faker->randomFloat(2, 40, 50);;
 
-        $new_flat->image = $faker->imageUrl(300, 300, 'apartment', true);
-       // $faker->image('public/storage/images',640,480, null, false);
 
-        
+            $new_flat->image = $faker->imageUrl(300, 300, 'apartment', true);
+            // $faker->image('public/storage/images',640,480, null, false);
 
-        $new_flat->visible = $faker->boolean();
 
-        $new_flat->save();
 
-     }
+            $new_flat->visible = $faker->boolean();
+
+            $new_flat->save();
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,9 +17,14 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!--Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -26,7 +32,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -50,13 +58,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -77,4 +86,5 @@
         </main>
     </div>
 </body>
+
 </html>
