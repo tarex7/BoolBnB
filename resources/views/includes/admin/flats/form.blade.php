b2a60f6c743f6dc1fbde27d8817d657e96fe7053
MarcoBeraldo <beraldomar@gmail.com>
2022-10-24 18:17:31 +0200

    tom tom api try

--- resources/views/includes/admin/flats/form.blade.php
+++ resources/views/includes/admin/flats/form.blade.php
@@ -133,8 +133,62 @@
         </div>
     @endforeach
 
+    <div class="form-group">
+        <label for="address">Indirizzo:</label>
+        <div id="address-tomtom">
+        </div>
+        <input id="lat" type="text" class="form-control" name="latitude" id="lat"
+            value="{{ old('latitude', $flat->latitude) }}" hidden>
+
+        <input id="lon" type="text" class="form-control" name="longitude" id="lon"
+            value="{{ old('longitude', $flat->longitude) }}" hidden>
+
+
+    </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
 </div>
 </div>
+<script>
+    var options = {
+        searchOptions: {
+            key: 'I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh',
+            language: 'it-IT',
+            limit: 10,
+        },
+        autocompleteOptions: {
+            key: 'I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh',
+            language: 'it-IT',
+        }
+    };
+    const latInput = document.getElementById('lat');
+    const lonInput = document.getElementById('lon');
+    const addressContainer = document.getElementById('address-tomtom')
+    var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+    var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+    addressContainer.append(searchBoxHTML);
+    const tomtomInput = document.getElementsByClassName("tt-search-box-input")[0];
+    let date = {}
+    ttSearchBox.on('tomtom.searchbox.resultsfound', function(data) {
+        date = (data);
+        let position = date.data.results.fuzzySearch.results[0].position;
+        let lon = position.lng;
+        let lat = position.lat;
+        latInput.value = lat;
+        lonInput.value = lon;
+    });
+    tomtomInput.setAttribute("name", "address");
+    tomtomInput.value = '<?php echo $flat->address; ?>';
+
+    let axios = require('axios').default;
+
+    addressContainer.addEventListener("keyup", (event) => {
+        axios.get(
+                `https://api.tomtom.com/search/2/autocomplete/${addressContainer.value}.json?key=I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh&language=it-IT&limit=6`
+            )
+            .then((res) => {
+                console.log(res.data);
+            })
+    });
+</script>
 </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,7 +19,45 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel='stylesheet' type='text/css'
+        href='https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css' />
+
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js">
+    </script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js">
+    </script>
+
+    <script>
+        (function() {
+            window.SS = window.SS || {};
+            SS.Require = function(callback) {
+                if (typeof callback === 'function') {
+                    if (window.SS && SS.EventTrack) {
+                        callback();
+                    } else {
+                        var siteSpect = document.getElementById('siteSpectLibraries');
+                        var head = document.getElementsByTagName('head')[0];
+                        if (siteSpect === null && typeof head !== 'undefined') {
+                            siteSpect = document.createElement('script');
+                            siteSpect.type = 'text/javascript';
+                            siteSpect.src = '/__ssobj/core.js+ssdomvar.js+generic-adapter.js';
+                            siteSpect.async = true;
+                            siteSpect.id = 'siteSpectLibraries';
+                            head.appendChild(siteSpect);
+                        }
+                        if (window.addEventListener) {
+                            siteSpect.addEventListener('load', callback, false);
+                        } else {
+                            siteSpect.attachEvent('onload', callback, false);
+                        }
+                    }
+                }
+            };
+        })();
+    </script>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -26,7 +65,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -50,13 +91,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -77,4 +119,5 @@
         </main>
     </div>
 </body>
+
 </html>
