4823b3ee863cf62ae3ccceac786e8a9929cf2426
tarex7 <tarex7@gmail.com>
2022-10-25 14:55:32 +0200

    added script to form

--- resources/views/includes/admin/flats/form.blade.php
+++ resources/views/includes/admin/flats/form.blade.php
@@ -206,6 +206,56 @@
             </form>
 
             <script>
+                var options = {
+                    searchOptions: {
+                        key: 'I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh',
+                        language: 'it-IT',
+                        limit: 10,
+                    },
+                    autocompleteOptions: {
+                        key: 'I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh',
+                        language: 'it-IT',
+                    }
+                };
+                const latInput = document.getElementById('lat');
+                const lonInput = document.getElementById('lon');
+                const addressContainer = document.getElementById('address-tomtom')
+            
+                const testbtn = document.getElementById('test');
+            
+                var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+                var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+                addressContainer.append(searchBoxHTML);
+                const tomtomInput = document.getElementsByClassName("tt-search-box-input")[0];
+                let date = {}
+                ttSearchBox.on('tomtom.searchbox.resultsfound', function(data) {
+                    date = (data);
+                    let position = date.data.results.fuzzySearch.results[0].position;
+                    let lon = position.lng;
+                    let lat = position.lat;
+                    latInput.value = lat;
+                    lonInput.value = lon;
+                });
+                tomtomInput.setAttribute("name", "address");
+                tomtomInput.value = '<?php echo $flat->address; ?>';
+               // console.log(addressContainer)
+            
+                // let axios = require('axios');
+            
+                
+            
+            
+                addressContainer.addEventListener("keydown", (e) => {
+                    // axios.get(
+                    //         `https://api.tomtom.com/search/2/autocomplete/${addressContainer.value}.json?key=I7jwOnv7XxCbU6AV64AN8ZPGArFaIoTh&language=it-IT&limit=6`
+                    //     )
+                    //     .then((res) => {
+                    //         console.log(res.data);
+                    //     })
+                });
+            </script>
+
+            <script>
                 const placeholder = {{ asset('images/placeholder.png')}}
                 const image = document.getElementById('image')
                 const preview = document.getElementById('preview')
