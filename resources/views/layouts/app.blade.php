74d66446d73dcb6b6f249aaae0a40fcb759bce65
tarex7 <tarex7@gmail.com>
2022-10-25 14:53:58 +0200

    added scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,73 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script type="text/javascript" src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js">
-    </script>
-
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <script src="{{ asset('js/delete_confirmation.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-
-    {{-- Font awesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css">
-
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    
+        {{-- Tomtom map --}}
+        <script type="text/javascript" src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps-web.min.js"></script>
+    
+        {{-- Tomtom searchbar --}}
+        <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
+        <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js"></script>
+        <script>
+            (function() {
+                window.SS = window.SS || {};
+                SS.Require = function(callback) {
+                    if (typeof callback === 'function') {
+                        if (window.SS && SS.EventTrack) {
+                            callback();
+                        } else {
+                            var siteSpect = document.getElementById('siteSpectLibraries');
+                            var head = document.getElementsByTagName('head')[0];
+                            if (siteSpect === null && typeof head !== 'undefined') {
+                                siteSpect = document.createElement('script');
+                                siteSpect.type = 'text/javascript';
+                                siteSpect.src = '/__ssobj/core.js+ssdomvar.js+generic-adapter.js';
+                                siteSpect.async = true;
+                                siteSpect.id = 'siteSpectLibraries';
+                                head.appendChild(siteSpect);
+                            }
+                            if (window.addEventListener) {
+                                siteSpect.addEventListener('load', callback, false);
+                            } else {
+                                siteSpect.attachEvent('onload', callback, false);
+                            }
+                        }
+                    }
+                };
+            })();
+        </script>
+    
+        {{-- Delete confirmation --}}
+        <script src="{{ asset('js/delete_confirmation.js') }}" defer></script>
+    
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    
+        {{-- Font awesome --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+        <!-- Styles -->
+        <link rel='stylesheet' type='text/css'
+            href='https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css' />
+        <link rel="stylesheet" href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.20.0/maps/maps.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    </head>
 
 <body class="bg-light">
     <div id="app">
