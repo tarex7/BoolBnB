d381fa6e5f66042b5e6282b21892757ac94ea3c7
torone89 <torone89>
2022-10-27 16:54:28 +0200

    feat: added bg

--- resources/views/admin/flats/create.blade.php
+++ resources/views/admin/flats/create.blade.php
@@ -2,12 +2,17 @@
 
 <div class="container-fluid">
 
-    @section('content')
-        <header class="my-5">
-            <h1 class="text-center mb-3 text-uppercase text-danger">Creazione appartamento</h1>
-        </header>
 
-        @include('includes.admin.flats.form')
+    @section('content')
+        <div class="bg-admin-flats-homepage py-5">
+            <header class="mb-5">
+                <h1 class="text-center mb-3 text-uppercase text-danger">Creazione appartamento</h1>
+
+            </header>
+
+
+            @include('includes.admin.flats.form')
+        </div>
     @endsection
 
 </div>
