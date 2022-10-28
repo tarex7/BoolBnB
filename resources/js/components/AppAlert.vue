f79b895fa5be0325e8f2be509e4dc7f96fa2a5a5
torone89 <torone89>
2022-10-28 19:33:31 +0200

    fea:add component Alert

---
+++ resources/js/components/AppAlert.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="alert fade show"
+    :class="[`alert-${type || 'info'}`, { 'alert-dismissible': dismissible }]"
+    role="alert"
+  >
+    <slot></slot>
+
+    <button type="button" class="close" @click="$emit('close')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAlert",
+  props: {
+    type: String,
+    dismissible: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
+
+
+
+
