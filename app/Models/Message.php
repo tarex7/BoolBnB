91c9e1399e91936803be1c3f6180c8b79c290908
torone89 <torone89>
2022-11-02 11:19:17 +0100

    feat: add flat_id

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
 
-    protected $fillable = ['sender_name', 'sender_email', 'text', 'object'];
+    protected $fillable = ['flat_id', 'sender_name', 'sender_email', 'text', 'object'];
     public function flat()
     {
         return $this->belongsTo('App\Models\Flat');
