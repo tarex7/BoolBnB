2860481d6245594766fb83ce1c2c73b67e4f0f7d
tarex7 <tarex7@gmail.com>
2022-11-04 19:05:45 +0100

    added appalert

---
+++ resources/js/components/AppAlert.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="alert fade show"
+    :class="[`alert-${type || 'info'}`, { 'alert-dismissible': dismissible }]"
+    role="alert"
+  >
+    <slot></slot>
+
+    <button type="button" class="close" @click="$emit('close')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAlert",
+  props: {
+    type: String,
+    dismissible: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
